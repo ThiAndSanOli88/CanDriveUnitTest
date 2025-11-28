e89b99ca7b8928ca624127c6dd7f0d7f319f7187
S-ThiagoAndre.San <s-thiagoandre.san@lwtech.edu>
2025-11-28 10:46:36 -0800

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CanDriveUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SCCM")]
+[assembly: AssemblyProduct("CanDriveUnitTest")]
+[assembly: AssemblyCopyright("Copyright © SCCM 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("272e869f-600b-4fbc-b410-a870b07d1aa3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CanDriveUnitTest
+{
+
+    [TestClass]
+    public class UnitTest1
+    {
+        public bool CanDrive(int age)
+        {
+            if (age <= 0 || age > 120)
+            {
+                throw new ArgumentOutOfRangeException(nameof(age), "Age must be between 1 and 120.");
+            }
+            const int drivingAge = 16;
+            return age >= drivingAge;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMethod1()
+        {
+            CanDrive(int.MinValue);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMethod2()
+        {
+            CanDrive(int.MinValue + 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMethod3()
+        {
+            CanDrive(-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMethod4()
+        {
+            CanDrive(0);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.IsFalse(CanDrive(1));
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.IsFalse(CanDrive(15));
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Assert.IsTrue(CanDrive(16));
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Assert.IsTrue(CanDrive(30));
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Assert.IsTrue(CanDrive(120));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]    
+        public void TestMethod10()
+        {
+            CanDrive(121);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMethod11()
+        {
+            CanDrive(int.MaxValue - 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMethod12()
+        {
+            CanDrive(int.MaxValue);
+        }
+    }
+}
